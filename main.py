a2beacfcee67db9645576ed85159847b9acd31a4
Virendraverma9950 <153084063+Virendraverma9950@users.noreply.github.com>
2024-04-02 22:17:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class Config(object):
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
 
-    AUTH_USERS = os.environ.get('AUTH_USERS', '1826828317').split(',')
+    AUTH_USERS = os.environ.get('AUTH_USERS', '1826828317', '6448990104').split(',')
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
